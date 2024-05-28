365e831b6908522025f8019c6047bfb28226d76a
Rochi <163904427+RochiC@users.noreply.github.com>
2024-05-28 09:40:35 -0300

    Password Check

--- Assets/LOGINMANAGER.cs
+++ Assets/LOGINMANAGER.cs
@@ -1,18 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LOGINMANAGER : MonoBehaviour
 {
+    //Cuando el usuario presione el boton BtnAceptar
+    //Debe escribirse un mensaje en la consola
+    //Si el texto del InputPassword coincide
+    //con una contraseña predefinida por ustedes
+    //escribir "Access Granted"
+    //Sino, escribir "Access Denied"
     // Start is called before the first frame update
+
+    public GameObject boton;
+    public Text HolderContraseña;
+    public string contraseña;
+   
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    public void login ()
     {
-        
+        if (HolderContraseña.text==contraseña)
+        {
+            Debug.Log("Access Granted");
+        }       
+        else
+        {
+            Debug.Log("Access Denied");
+        }
     }
 }
